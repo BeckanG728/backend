da159591dc4edff5b136dea3c38a5761c414cc8c
Bkn <beckan0011@gmail.com>
2025-11-11 10:54:15 -0500

    Gfs commit

--- src/main/java/com/tpdteam3/backend/config/CorsConfig.java
+++ src/main/java/com/tpdteam3/backend/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
                         .allowedOrigins(
                                 "http://localhost:5500",
                                 "http://127.0.0.1:5500",
-                                "https://www.tpdteam3.com"
+                                "https://www.tpdteam3.com",
+                                "https://tpdteam3.com"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
